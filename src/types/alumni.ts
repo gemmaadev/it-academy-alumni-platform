cf0264ce30e7b7290404657fb24514cc26655da0
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-29 09:11:43 +0200

    feat: add Alumni interface and fetchAlumni service with My JSON Server API

---
+++ src/types/alumni.ts
@@ -0,0 +1,10 @@
+export interface Alumni {
+  id: number;
+  firstName: string;
+  lastName: string;
+  position: string;
+  company: string;
+  classOf: number;
+  location: string;
+  avatar: string;
+}
