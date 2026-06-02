52dcf62751802e3e26bb420e68065d657c6f4f1a
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-02 20:34:17 +0200

    fix: update footer to work precisely with router

--- src/logic/footer.ts
+++ src/logic/footer.ts
@@ -4,10 +4,10 @@ export function setupFooter(page: string): void {
   const bottomNav = document.getElementById("bottom-nav");
 
   if (page === "login" || page === "splash-page") {
-    if (footer) footer.style.display = "none";
-    if (bottomNav) bottomNav.style.display = "none";
+    if (footer) footer.classList.add("hidden");
+    if (bottomNav) bottomNav.classList.add("hidden");
   } else {
-    if (footer) footer.style.display = "block";
-    if (bottomNav) bottomNav.style.display = "flex";
+    if (footer) footer.classList.remove("hidden");
+    if (bottomNav) bottomNav.classList.remove("hidden");
   }
 }
