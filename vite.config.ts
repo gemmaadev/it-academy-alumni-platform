ce869933fd05572a47ced88970b3ec23bf3555fa
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-04 22:27:50 +0200

    fix: copy src/pages, components and styles to dist for Netlify deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
+  base: "/",
   plugins: [
     viteStaticCopy({
       targets: [
         { src: "src/pages/*", dest: "src/pages" },
         { src: "src/components/*", dest: "src/components" },
+        { src: "src/styles/*", dest: "src/styles" },
       ],
     }),
   ],
